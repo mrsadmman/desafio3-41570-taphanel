cab5b1ce6fc374e4eaf1aa0be099cdfa3d6fbcf6
Juan Taphanel <taphanel.juan@gmail.com>
2022-08-17 12:25:49 -0300

    Solucionado el ESC, y agregue Menu 6 para total de comensales

--- main.js
+++ main.js
@@ -25,24 +25,20 @@ function plataDonada() {
   return parseInt(plata);
 }
 function menu() {
-  let seleccionMenu = prompt('Seleccione Menu\n 1:Donar plata\n 2:Cantidad Comensales\n 3:Ubicacion del Comedor\n 4:Registrar un nuevo comedor\n 5:Mostrar nombres Comedores');
+  let seleccionMenu = prompt(
+    'Seleccione Menu\n 1:Donar plata\n 2:Cantidad Comensales\n 3:Ubicacion del Comedor\n 4:Registrar un nuevo comedor\n 5:Mostrar nombres Comedores\n 6:Total de Comensales\n 0:Salir'
+  );
   return parseInt(seleccionMenu);
 }
 const comedor1 = new Comedor('El Gomero', 50, 'Barrancas de Belgrano');
 const comedor2 = new Comedor('Manitos', 30, 'Parque Centenario');
 const comedor3 = new Comedor('Doña Tota', 60, 'Villa Fiorito');
 
-/* AGREGAR UN NUEVO COMEDOR POR ARRAY */
-/* const comedores = [comedor1, comedor2, comedor3];
-comedores.push(new Comedor(prompt('Ingrese nombre comedor'), prompt('Ingrese Cantidad de Comensales'), prompt('Ingrese Barrio')));
-console.log(comedores); */
-/* ---------------------------------- */
-
 let menuSeleccionado = '';
 const comedores = [comedor1, comedor2, comedor3];
 
 // SELECCIONE MENU
-while ((menuSeleccionado = menu()) != 'ESC') {
+while ((menuSeleccionado = menu()) != 0) {
   switch (menuSeleccionado) {
     case 1:
       //MENU DONAR PLATA
@@ -114,10 +110,18 @@ while ((menuSeleccionado = menu()) != 'ESC') {
     //MENU MOSTRAR NOMBRE COMEDORES
     case 5:
       const nombresCom = comedores.map((el) => {
-        //LO CAMBIA POR OTRO
         return el.nombre;
       });
       alert(nombresCom);
+      break;
+    //MENU TOTAL DE COMENSALES
+    case 6:
+      const totalComedor = comedores.reduce((acc, item) => {
+        alert('El comedor ' + item.nombre + ' ' + 'tiene ' + item.cantidad + ' ' + 'comensales');
+        return (acc += item.cantidad);
+      }, 0);
+
+      alert('La cantidad total de comensales es de: ' + totalComedor);
       break;
     default:
       alert('Menu incorrecto, vuelva a intentarlo');
